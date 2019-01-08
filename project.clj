0a5088f5fc2d3d4c6ea8dbbc2c619edc116402f1
Joe Wass <jwass@crossref.org>
2019-01-08 18:29:29 +0000

    Add Dockerfile for running locally.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://eventdata.crossref.org"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [event-data-common "0.1.7"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [event-data-common "0.1.60"]
                  [yogthos/config "0.8"]
                  [clj-time "0.12.2"]]
   :main ^:skip-aot event-data-artifact-manager.core
